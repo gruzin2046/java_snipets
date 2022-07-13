c28b939b031146b7116d74d5ba6b524a4ce27c31
aluniewi <albert.luniewicz@capgemini.com>
2022-07-13 22:06:34 +0200

    rounded double + iterator

--- src/_19_double_rounded.java
+++ src/_19_double_rounded.java
@@ -1,5 +1,4 @@
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
 
 public class _19_double_rounded {
---
+++ src/_20_Iterator.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class _20_Iterator {
+    public static void main(String[] args) {
+
+        String[] names = {"ala","ada","asia","kasia","beata","kamil"};
+        ArrayList<String> nameList = new ArrayList<>(Arrays.asList(names));
+
+        // create iterator based on list.
+        // iterator is a pointer who point certain element on the list.
+        // we can use iterator to remove element from dynamic list,
+        // without problems with index.
+        Iterator<String> iterator = nameList.iterator();
+
+        // use while + iterator.hasNext() to walk through the list
+        // hasNext() is returning boolean
+        while (iterator.hasNext()) {
+            String currentElement = iterator.next();
+            if (currentElement.equals("beata")) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println(nameList);
+    }
+}
