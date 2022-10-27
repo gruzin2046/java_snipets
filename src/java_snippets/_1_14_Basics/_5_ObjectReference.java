01e2b42f0aa027e074fc8b22183fb633332a9291
aluniewi <albert.luniewicz@capgemini.com>
2022-10-27 12:35:41 +0200

    basics refactor/additions

--- src/java_snippets/_1_14_Basics/_5_ObjectReference.java
+++ src/java_snippets/_1_14_Basics/_5_ObjectReference.java
@@ -18,15 +18,25 @@ public class _5_ObjectReference {
             Person borrower;
         }
 
+        // 1. create reference from borrower to person1 object
 
         Person person1 = new Person("Jan", 32);
         Credit credit1 = new Credit();
         credit1.amount = 200;
-
-        // create reference from borrower to person1 object
         credit1.borrower = person1;
 
         System.out.println("Borrower " + credit1.borrower.name + " borrow " + credit1.amount + " $");
+        // Prints: Borrower Jan borrow 200 $
 
+        // 2. assign different values via different reference to the same instance
+
+        Person personX = new Person("Brian", 12);
+        Person personY = new Person("Brian", 12); // creates different object than personX (new)
+        Person personZ = personY; // personZ points the same instance as personY
+
+        personY.age = 50;
+        personZ.age = 100;
+
+        System.out.println(personY.age); // Prints: 100 because personZ refers to the same object as personY
     }
 }
--- src/java_snippets/_30_Collections/List_.java
+++ src/java_snippets/_30_Collections/List_.java
@@ -13,14 +13,14 @@ public class List_ {
         LinkedList<String> linkedList = new LinkedList<>(Arrays.asList(names));
 
         /* Methods:
-        .add(index, element), .add(element), addFirst(element), addLast(element),
-        .remove(), .remove(Object), .remove(index), .removeFirst(), .removeLast(),
-        .removeFirstOccurrence(Object), .removeLastOccurrence(Object),
-        .get(index), .getFirst(), .getLast(), .set(index, .element),
-        .clone(), .clear(), .contains(Object), .size(),
-        .pop() - remove and return the top element from the stack,
-        .push(element),
-        .indexOf(Object), .lastIndexOf(Object) .toArray(), toString()
+        .add(index, element) .add(element) addFirst(element) addLast(element)
+        .remove() .remove(Object) .remove(index) .removeFirst() .removeLast()
+        .removeFirstOccurrence(Object) .removeLastOccurrence(Object)
+        .get(index) .getFirst() .getLast() .set(index, .element)
+        .clone() .clear() .contains(Object) .size()
+        .pop() - remove and return the top element from the stack
+        .push(element)
+        .indexOf(Object) .lastIndexOf(Object) .toArray() toString()
         */
 
         List<String> namesStartsWithA = linkedList.stream()
